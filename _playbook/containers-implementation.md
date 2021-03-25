4303a67e3ee487e84085922cbdf71e48708f603e
ModernizationDigital <79791193+ModernizationDigital@users.noreply.github.com>
2021-03-25 13:02:34 -0400

    inputing chart

--- _playbook/containers-implementation.md
+++ _playbook/containers-implementation.md
@@ -8,6 +8,44 @@ title: 'Implementation'
 First of all, What do you want to do with your application to modernize it? [Link to section about Keep, Retire, Repurchase]. 
 
 The first thing to determine is Do you want to keep your application? If the answer is yes. Have you looked at what is available for modernization. Is there anything else out there that can do what your app can do? A Saas already made.
+- - -
+
+<html lang="en">
+
+  <head>
+    <title>Chart.js Bar Chart Template by handsondataviz.org</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+
+    <!-- Load jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  
+    <!-- Load Chart.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+
+    <!-- Load PapaParse to read csv files -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js"></script>
+  </head>
+
+  <body>
+
+    <canvas id="container" style="width:100%; height:400px;"></canvas>
+    
+    <p style="font-family: Helvetica; color: #333; padding: 1em 2em; font-size: 12px">
+      Source: www.idc.com Gary Chen June 2019 | Sponsored by: Amazon Web Services 
+      <br>
+      <a href="https://github.com/HandsOnDataViz/chartjs-bar">View data and code</a>,
+      created by <a href="https://handsondataviz.org/">HandsOnDataViz</a>
+      with <a href="https://www.chartjs.org/">Chart.js</a>
+    </p>
+
+    <script src="script.js"></script>
+
+  </body>
+
+</html>
+
+- - -
 
 ### Application migration
 
---
+++ js/script.js
@@ -0,0 +1,129 @@
+$(document).ready(function() {
+
+  var TITLE = 'Top Drivers of Containerization';
+
+  // `false` for vertical column chart, `true` for horizontal bar chart
+  var HORIZONTAL = true;
+
+	// `false` for individual bars, `true` for stacked bars
+  var STACKED = false;  
+  
+  // Which column defines 'bucket' names?
+  var LABELS = 'district';  
+
+  // For each column representing a data series, define its name and color
+  var SERIES = [  
+    {
+      column: 'nonlearner',
+      name: 'Non-Learners',
+      color: 'grey'
+    },
+    {
+      column: 'learner',
+      name: 'Learners',
+      color: 'blue'
+    }
+  ];
+
+  // x-axis label and label in tooltip
+  var X_AXIS = 'Percentage';
+
+  // y-axis label, label in tooltip
+  var Y_AXIS = 'Reasons';
+
+  // `true` to show the grid, `false` to hide
+  var SHOW_GRID = true; 
+
+  // `true` to show the legend, `false` to hide
+  var SHOW_LEGEND = true; 
+
+  // Read data file and create a chart
+  $.get('./data.csv', function(csvString) {
+
+    var rows = Papa.parse(csvString, {header: true}).data;
+
+    var datasets = SERIES.map(function(el) {
+      return {
+        label: el.name,
+        labelDirty: el.column,
+        backgroundColor: el.color,
+        data: []
+      }
+    });
+
+    rows.map(function(row) {
+      datasets.map(function(d) {
+        d.data.push(row[d.labelDirty])
+      })
+    });
+
+		var barChartData = {
+      labels: rows.map(function(el) { return el[LABELS] }),
+			datasets: datasets
+    };
+
+    var ctx = document.getElementById('container').getContext('2d');
+
+    new Chart(ctx, {
+      type: HORIZONTAL ? 'horizontalBar' : 'bar',
+      data: barChartData,
+      
+      options: {
+        title: {
+          display: true,
+          text: TITLE,
+          fontSize: 14,
+        },
+        legend: {
+          display: SHOW_LEGEND,
+        },
+        scales: {
+          xAxes: [{
+            stacked: STACKED,
+            scaleLabel: {
+              display: X_AXIS !== '',
+              labelString: X_AXIS
+            },
+            gridLines: {
+              display: SHOW_GRID,
+            },
+            ticks: {
+              beginAtZero: true,
+              callback: function(value, index, values) {
+                return value.toLocaleString();
+              }
+            }
+          }],
+          yAxes: [{
+            stacked: STACKED,
+            beginAtZero: true,
+            scaleLabel: {
+              display: Y_AXIS !== '',
+              labelString: Y_AXIS
+            },
+            gridLines: {
+              display: SHOW_GRID,
+            },
+            ticks: {
+              beginAtZero: true,
+              callback: function(value, index, values) {
+                return value.toLocaleString()
+              }
+            }
+          }]
+        },
+        tooltips: {
+          displayColors: false,
+          callbacks: {
+            label: function(tooltipItem, all) {
+              return all.datasets[tooltipItem.datasetIndex].label
+                + ': ' + tooltipItem.yLabel.toLocaleString();
+            }
+          }
+        }
+      }
+    });
+
+  });
+
+});
